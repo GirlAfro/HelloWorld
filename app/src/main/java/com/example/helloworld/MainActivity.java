61ca1c15cbac986d223e2173376e96f5d4779310
GirlAfro <kimchigarira13@gmail.com>
2024-10-02 11:01:58 +0300

    added functionality to btnChangeText

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onBtnChangeTextClick(View view){
+        TextView tvChangeText =findViewById(R.id.tvChangeText);
+        tvChangeText.setText("You Are A Star💫");
+
     }
 }
